8224496d97b5696b7ce55970e71919764350a78e
Michael Lynch <michael.lynch8211@gmail.com>
2020-10-15 21:58:03 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
       if !params[:name].nil?
         session[:name] = params[:name]
-        redirect_to '/new'
+        redirect_to '/'
     else
       redirect_to '/login'
     end
