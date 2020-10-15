3939b619aca8e191c47cad3bb6dd00558e4e8b25
Michael Lynch <michael.lynch8211@gmail.com>
2020-10-15 21:54:07 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if !params[:name].nil?
-      session[:name] = params[:name]
-    redirect_to '/'
-  else
-    redirect_to '/login'
-  end
+      if !params[:name].nil?
+        session[:name] = params[:name]
+        redirect_to '/'
+    else
+      redirect_to '/login'
+    end
   end
 
   def destroy
